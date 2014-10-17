eee64eebfa29e8e78ffc95ee94f4bd40a20bda1b
aleksandar-mitrevski <aleksandar.mitrevski.1990@gmail.com>
2014-10-17 23:37:24 +0200

    This seems to be a better implementation of the hybrid velocity counter

--- hybrid_velocity_counter/testbench.vhd
+++ hybrid_velocity_counter/testbench.vhd
@@ -4,13 +4,15 @@ use IEEE.std_logic_1164.All;
 entity testbench is end testbench;
 
 architecture tb_velocity of testbench is
+    signal clk : std_logic := '0';
     signal encoder : std_logic := '0';
     signal speed : real;
     signal measurementType : std_logic;
 
-    constant twenty_five_nsec : time := 25 ns;
+    constant twenty_nsec : time := 20 ns;
 
     component HybridVelocityCounter port (
+        clk : in std_logic;
         encoder : in std_logic;
         speed : out real;
         measurementType: out std_logic);
@@ -19,8 +21,30 @@ architecture tb_velocity of testbench is
 begin
     HybridVelocityCounter1 : HybridVelocityCounter
     port map (
+        clk => clk,
         encoder => encoder,
         speed => speed,
         measurementType => measurementType);
+
+    create_twenty_Mhz: process
+    begin
+        wait for twenty_nsec;
+        clk <= NOT clk;
+    end process;
+
+    encoder <= '1' after 300 ns,
+               '0' after 500 ns,
+               '1' after 700 ns,
+               '0' after 900 ns,
+               '1' after 1100 ns,
+               '0' after 1300 ns,
+               '1' after 1500 ns,
+               '0' after 1700 ns,
+               '1' after 1900 ns,
+               '0' after 2100 ns,
+               '1' after 2700 ns,
+               '0' after 3300 ns,
+               '1' after 3900 ns,
+               '0' after 4500 ns;
 
 end tb_velocity;
--- hybrid_velocity_counter/velocity_counter.vhd
+++ hybrid_velocity_counter/velocity_counter.vhd
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.All;
 
 entity HybridVelocityCounter is port (
+    clk : in std_logic;
     encoder : in std_logic;
     speed : out real;
     measurementType: out std_logic);
@@ -9,27 +10,24 @@ end HybridVelocityCounter;
 
 architecture velocity of HybridVelocityCounter is
     type measurementTypes is (HighSpeed, LowSpeed);
-    constant samplingPeriod: time := 100 ns; --sampling period in nanoseconds
+    constant clockFrequency : integer := 20; -- frequency in nanoseconds
+    constant samplingPeriod: integer := 1500; -- sampling period in nanoseconds
     constant minimumHighSpeedCounter : integer := 5;
-    constant maximumLowSpeed : real := 0.05;
+    constant maximumLowSpeed : real := 0.01;
 
     signal currentMeasurementType : measurementTypes := HighSpeed;
 
 begin
-    speedMeasurement: process
+    speedMeasurement: process(clk)
         variable pulseCounter : integer := 0;
-        variable startTime : time := 0 ns;
-        variable timeChange : time := 0 ns;
+        variable timeChange : integer := 0;
         variable counted : std_logic;
         variable timeReal : real;
         variable currentSpeed : real;
+        variable measuringTime : std_logic;
     begin
-        if currentMeasurementType = HighSpeed then
-            startTime := now;
-            timeChange := 0 ns;
-            counted := '0';
-
-            while timeChange < samplingPeriod loop
+        if rising_edge(clk) then
+            if currentMeasurementType = HighSpeed then
                 if encoder = '1' then
                     if counted = '0' then
                         pulseCounter := pulseCounter + 1;
@@ -38,41 +36,61 @@ begin
                 elsif counted = '1' then
                     counted := '0';
                 end if;
-                timeChange := now - startTime;
-            end loop;
 
-            --we switch to a low-velocity mode if the pulse counter
-            --is below a predefined threshold
-            if pulseCounter < minimumHighSpeedCounter then
-                currentMeasurementType <= LowSpeed;
+                timeChange := timeChange + 2 * clockFrequency;
+                if timeChange >= samplingPeriod then
+                    --we switch to a low-velocity mode if the pulse counter
+                    --is below a predefined threshold
+                    if pulseCounter < minimumHighSpeedCounter then
+                        currentMeasurementType <= LowSpeed;
+                        measurementType <= '1';
+                    else
+                        measurementType <= '0';
+                    end if;
+
+                    timeReal := real(samplingPeriod);
+                    speed <= real(pulseCounter) / timeReal;
+
+                    -- we reset the variables to allow for new measurements
+                    pulseCounter := 0;
+                    timeChange := 0;
+                    counted := '0';
+                end if;
+            else
+                if encoder = '1' then
+                    if measuringTime = '1' then
+                        timeChange := timeChange + 2 * clockFrequency;
+                        if counted = '0' then
+                            timeReal := real(timeChange);
+                            currentSpeed := 1.0 / timeReal;
+
+                            if currentSpeed > maximumLowSpeed then
+                                currentMeasurementType <= HighSpeed;
+                                measurementType <= '0';
+                            else
+                                measurementType <= '1';
+                            end if;
+
+                            speed <= currentSpeed;
+
+                            -- we reset the variables to allow for new measurements
+                            timeChange := 0;
+                            counted := '0';
+                            measuringTime := '0';
+                        end if;
+                    else
+                        counted := '1';
+                        measuringTime := '1';
+                    end if;
+                else
+                    if measuringTime = '1' then
+                        timeChange := timeChange + 2 * clockFrequency;
+                    end if;
+                    if counted = '1' then
+                        counted := '0';
+                    end if;
+                end if;
             end if;
-
-            timeReal := real(samplingPeriod / 1 ns);
-            speed <= real(pulseCounter) / timeReal;
-            measurementType <= '0';
-
-            --we reset the pulse counter
-            pulseCounter := 0;
-        else
-            if encoder = '1' then
-                wait until encoder = '0';
-            end if;
-
-            wait until encoder = '1';
-            startTime := now;
-            wait until encoder = '0';
-            wait until encoder = '1';
-            timeChange := now - startTime;
-
-            timeReal := real(timeChange / 1 ns);
-            currentSpeed := 1.0 / timeReal;
-
-            if currentSpeed > maximumLowSpeed then
-                currentMeasurementType <= HighSpeed;
-            end if;
-
-            speed <= currentSpeed;
-            measurementType <= '1';
         end if;
     end process;
 end velocity;
