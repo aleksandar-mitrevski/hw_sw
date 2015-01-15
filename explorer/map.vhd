96d50e88acb6caad44d2994de4bb7ec7a48d213a
aleksandar-mitrevski <aleksandar.mitrevski.1990@gmail.com>
2015-01-15 19:39:40 +0100

    Updated xploration files; the code has not been tested yet and bugs/errors are possible in this version

--- explorer/map.vhd
+++ explorer/map.vhd
@@ -3,28 +3,19 @@ use ieee.std_logic_1164.all;
 use work.exploration_pkg.all;
 
 entity ExplorationGrid is port (
-    currentX: in real;
-    currentY: in real;
+    currentCellsInView: in gridArray;
     grid : out gridArray);
 end ExplorationGrid;
 
 architecture explorationGrid of ExplorationGrid is
     signal exploredGrid : gridArray;
-    signal currentXPosition : real;
-    signal currentYPosition : real;
 begin
-
-    process(currentX, currentY)
+    
+    process(currentCellsInView)        
     begin
-        currentXPosition <= currentX;
-        currentYPosition <= currentY;
-    end process;
-
-    process(currentXPosition, currentYPosition)
-        variable currentGridCoordinates : integer_array(0 to 1);
-    begin
-        currentGridCoordinates := worldToGrid(currentXPosition, currentYPosition);
-        exploredGrid(currentGridCoordinates(0))(currentGridCoordinates(1)) <= '1';
+        for i in 0 to currentCellsInView'length-1 loop
+            exploredGrid(i) <= '1';
+        end loop;
     end process;
 
     process(exploredGrid)
